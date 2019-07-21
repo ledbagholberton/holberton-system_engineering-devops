7d03486bc1e15963887d796f4c8427b667a61ebf
Giovanny Perez <789@holbertonschool.com>
2019-07-21 22:38:44 +0000

    Draws and others

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:|to:|flags:)(\+?[\d]{11})?([A-Za-z]+)?(\-?[0-1]:{5})?/).join(",")
+puts ARGV[0].scan(/(?<=from:|to:|flags:)(\+?[\d]{11})?([A-Za-z]+)?(((.?[0-1].){4})(.?[0-1]))?/).join(",")
