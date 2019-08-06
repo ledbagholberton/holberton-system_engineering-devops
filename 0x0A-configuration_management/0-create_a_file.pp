df1d530d71f7ea1d16fd17c698368de40421828c
root <789@holbertonschool.com>
2019-08-06 17:25:08 +0000

    Change without Exec

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,5 @@
-$doc_root = '/tmp/holberton'
-
-exec { 'create_file':
-  command => '/bin/echo -n I love Puppet > /tmp/holberton'
-}
-
-file { $doc_root:
+file { '/tmp/holberton':
+  content => 'I love Puppet',
   ensure => 'file',
   owner  => 'www-data',
   group  => 'www-data',
