8d48b90d8b0874f462ff6ef55c566a3c8be3d49b
root <789@holbertonschool.com>
2019-08-06 17:17:24 +0000

    FIxing enter

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 $doc_root = '/tmp/holberton'
 
 exec { 'create_file':
-  command => '/bin/echo I love Puppet > /tmp/holberton'
+  command => '/bin/echo -n I love Puppet > /tmp/holberton'
 }
 
 file { $doc_root:
