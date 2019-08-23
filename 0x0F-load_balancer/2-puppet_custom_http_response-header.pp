33aae37ac37c74957e616c545e75ba6eeabfd7e4
Abdel <789@holbertonschool.com>
2019-08-23 22:38:04 +0000

    Fix format, quotes, colons amd others

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,18 +1,19 @@
 #Header HTTP creation
 exec {'ensure update':
-command => 'usr/bin/apt-get update'
+command  => 'sudo apt update',
+provider => shell
 }
 
 -> package {'nginx':
   ensure => 'installed'
-}
+  }
 
 -> file_line {'header':
-path => 'etc/nginx/nginx.conf',
+path  => '/etc/nginx/nginx.conf',
 match => 'http {',
-line => "http {\n\tadd_header X-Served-By ${hostname};",
+line  => "http {\n\tadd_header X-Served-By ${hostname};"
 }
 
 -> exec {'start':
-command => 'usr/sbin/service nginx restart',
+command => '/usr/sbin/service nginx restart'
 }
