b5257a59ba3d87f044d6517df2ab72dd169d7079
ledbagholberton <789@holbertonschool.com>
2019-09-20 02:13:34 +0000

    Change Try/Except by If/Else + pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     client = requests.session()
     client.headers = headers
-    r = client.get(url, allow_redirects = False)
+    r = client.get(url, allow_redirects=False)
     if r.status_code == 200:
         return (r.json()["data"]["subscribers"])
     else:
