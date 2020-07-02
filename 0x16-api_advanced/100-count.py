6ccf9779c66ad93d01ad13204035b0690445caf3
Giovanny Perez <47123404+ledbagholberton@users.noreply.github.com>
2020-07-01 22:38:52 -0500

    Update 100-count.py
    Changes to test the new file

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,4 +1,56 @@
 #!/usr/bin/python3
+""" Functions to adcquire info from API Reddit"""
+import requests
+after = None
+
+
+def count_words(subreddit, word_list):
+    """Count the titles found with wordlist in subreddit"""
+    my_list = recurse(subreddit)
+    my_dict = {}
+
+    if my_list:
+        for word in word_list:
+            my_dict[word] = 0
+
+        for title in my_list:
+            title_split = title.split(" ")
+
+            for iter in title_split:
+                for iter_split in word_list:
+                    if iter.lower() == iter_split.lower():
+                        my_dict[iter_split] += 1
+
+        for key, val in sorted(my_dict.items(),  key=lambda x: x[1],
+                               reverse=True):
+            if val != 0:
+                print("{}: {}".format(key, val))
+
+
+def recurse(subreddit, hot_list=[]):
+    """ recurse is a function that return hot list from
+        a subreddit"""
+    global after
+    headers = {'User-Agent': 'ledbag123'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
+    if response.status_code == 200:
+        prox = response.json().get('data').get('after')
+
+        if prox is not None:
+            after = prox
+            recurse(subreddit, hot_list)
+        list_titles = response.json().get('data').get('children')
+
+        for title_ in list_titles:
+            hot_list.append(title_.get('data').get('title'))
+        return hot_list
+    else:
+        return (None)
+'''    
+#!/usr/bin/python3
 """ Exporting csv files"""
 import json
 import requests
@@ -36,3 +88,4 @@ def count_words(subreddit, word_list, after="null", host_list=[]):
                 print("{}: {}".format(word_list[pos], my_count[pos]))
     else:
         return(None)
+'''
